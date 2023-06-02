76bc2a48f8f370ff71904539d6b41a8c53bafc81
suzu <83053337+suzu0527@users.noreply.github.com>
2023-06-02 06:50:47 +0000

    Update 4 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import "./styles.css";
-
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+ /**
+  * const, letなどの変数宣言
+  */
+  
+  var val1 = "var変数";
+  console.log(val1);
+  
+  //var変数は上書き可能
+  val1 = "var変数を上書き";
+  console.log(val1);
+  
+  //var変数は再宣言可能
+  var val1 = "var変数を再宣言";
+  console.log(val1);
